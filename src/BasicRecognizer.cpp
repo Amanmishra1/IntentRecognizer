3bc64ba409d1de28ae00e5180c1b42cf2113331f
Aman Mishra <amanmishra1@gmail.com>
2020-08-20 20:01:39 +0200

    replace push_back with emplace_back

--- src/BasicRecognizer.cpp
+++ src/BasicRecognizer.cpp
@@ -2,11 +2,11 @@
 
 BasicRecognizer::BasicRecognizer()
 {  
-    basic_intent_.push_back(std::make_pair(std::regex(R"(What('s| is) the weather like( today| tomorrow| in (two|three|four|five|six) days)?\?)",std::regex::icase), Intent::kGet_Weather));
-    basic_intent_.push_back(std::make_pair(std::regex(R"(What('s| is) the weather like in (Munich|Berlin|Stuttgart|Paris)( today| tomorrow| in (two|three|four|five|six) days)?\?)",std::regex::icase), Intent::kGet_Weather_City));
-    basic_intent_.push_back(std::make_pair(std::regex(R"(Am I free at .* (AM|PM)( today| tomorrow)?\?)",std::regex::icase), Intent::kCheck_Calender));
-    basic_intent_.push_back(std::make_pair(std::regex("(.*interesting fact.*)",std::regex::icase), Intent::kGet_Fact));
-    basic_intent_.push_back(std::make_pair(std::regex("(.*)",std::regex::icase), Intent::kUnknown));
+    basic_intent_.emplace_back(std::make_pair(std::regex(R"(What('s| is) the weather like( today| tomorrow| in (two|three|four|five|six) days)?\?)",std::regex::icase), Intent::kGet_Weather));
+    basic_intent_.emplace_back(std::make_pair(std::regex(R"(What('s| is) the weather like in (Munich|Berlin|Stuttgart|Paris)( today| tomorrow| in (two|three|four|five|six) days)?\?)",std::regex::icase), Intent::kGet_Weather_City));
+    basic_intent_.emplace_back(std::make_pair(std::regex(R"(Am I free at .* (AM|PM)( today| tomorrow)?\?)",std::regex::icase), Intent::kCheck_Calender));
+    basic_intent_.emplace_back(std::make_pair(std::regex("(.*interesting fact.*)",std::regex::icase), Intent::kGet_Fact));
+    basic_intent_.emplace_back(std::make_pair(std::regex("(.*)",std::regex::icase), Intent::kUnknown));
 }
 
 const Intent BasicRecognizer::getIntent(const std::string &input_sentence) const
